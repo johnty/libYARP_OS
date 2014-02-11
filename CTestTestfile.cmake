c72e449e3854a860b28aedad72539418cabedc2e
johnty <johntywang@gmail.com>
2014-02-11 15:49:29 -0800

    adding gitignore

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: D:/yarp-2.3.22/src/libYARP_OS
-# Build directory: D:/yarp-2.3.22/src/libYARP_OS
+# Source directory: D:/dev/yarp-2.3.22/src/libYARP_OS
+# Build directory: D:/dev/yarp-2.3.22/src/libYARP_OS
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
